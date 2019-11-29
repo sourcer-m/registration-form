255610d5e91182cd061318a32708e295e0f704fa
eladgel <elad@honeybook.com>
2019-11-28 23:24:15 -0800

    Update dataUtils.js

--- dataUtils.js
+++ dataUtils.js
@@ -1,5 +1,15 @@
+function getToday() {
+  let date = new Date();
+  const nbsp = String.fromCharCode(160);
+  let dateStr = `${date.getDate()}${nbsp}/${nbsp}${date.getMonth() +1}${nbsp}/${nbsp}${date.getFullYear() - 2000}`;
 
-// var TODAY_STR = moment().format('D.M.YY');
+  if (Math.random() > 0.5) {
+    dateStr =  `${date.getDate()}.${date.getMonth() +1}.${date.getFullYear() - 2000}`;
+  }
+  return dateStr;
+}
+
+var TODAY_STR = getToday();
 var DAY_STR = ' ';
 var MONTH_STR = ' ';
 var YEAR_STR = TODAY_STR;
