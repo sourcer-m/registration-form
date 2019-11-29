c42c267b2f13627d13ad4981865a865d9970ea24
eladgel <elad@honeybook.com>
2019-11-28 23:08:49 -0800

    Update dataUtils.js

--- dataUtils.js
+++ dataUtils.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
-var TODAY_STR = moment().format('D.M.YY');
+// var TODAY_STR = moment().format('D.M.YY');
 var DAY_STR = ' ';
 var MONTH_STR = ' ';
 var YEAR_STR = TODAY_STR;
-var YEAR_INT = moment().format('YYYY');
+var YEAR_INT = (new Date()).getFullYear();
